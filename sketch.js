52ca9856c01d2eee412465cfee248583fee27d33
no <47332329+VEESCREAMS@users.noreply.github.com>
2019-05-28 14:15:48 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(600, 700);
+}
+
+function draw() {
+  background(255, 133, 96);
+  fill(245, 252, 111);
+  textSize(30)
+  text("THE END!", 300, 50)
+  textSize(80)
+  text("~ - . - ~", 245, 120)
+  textSize(30)
+  text("Congradulations! You have successfully", 75, 190);
+  text("collected ITEM NAME", 220,220);
+  text("Your friend is very thankful, and the", 130, 250);
+  text("adventure seems worth it after seeing", 97 ,280);
+  text("their happy expression!", 222, 310);
+}
