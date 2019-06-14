4217d83ac48592b3340d937b8b2fe06d26dc2398
ULT01 <47332107+ULT01@users.noreply.github.com>
2019-06-14 15:23:27 -0400

    Update sketchfinal.js

--- sketchfinal.js
+++ sketchfinal.js
@@ -1,50 +1,41 @@
 // let song;
 
 let img;
-let i;
 let m;
+let y;
+
 let bug;
 
 function setup() {
    // song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
   createCanvas(1920, 1200);
-  img = loadImage('20190514_001-4.jpg');
+  img = loadImage('20190514_001-5.jpg'); 
   
-  i = loadImage('20190514_001-3.jpg');
+  m = loadImage('20190514_001-4.jpg');
   
-  m = loadImage('20190514_001-5.jpg');
-
-
+  y = loadImage('20190514_001-3.jpg');
+  
   se1.draw();
   btn1.draw();
   bug = new Jitter();
 }
 
 function draw() {
- 
-   image(i, 0, height / 2, i.width / 4, i.height / 4);
-  
-   image(m, 0, height / 4, m.width / 4, m.height / 4);
-  
   
 
-
-
-
+  image(m, 0, height / 4, m.width / 4, m.height / 4);
+  
+  image(y, 0, height / 2, img.width / 4, img.height / 4);
   
  if (scene > 0 && scene  < 5) {
    bug.move();
    bug.display();
    bug.update();
+     image(m, 0, height / 2, m.width / 4, m.height / 4);
    
-   image(img, 0, height / 2, img.width / 4, img.height / 4);
    
-   image(img, 0, height / 2, img.width / 4, img.height / 4);
-
  } 
   
 }
-
-
 
 
