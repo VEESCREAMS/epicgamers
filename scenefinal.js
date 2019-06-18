0d7a470a2f80854995e96f2576cdc9dd6f287b20
ULT01 <47332107+ULT01@users.noreply.github.com>
2019-06-18 14:17:53 -0400

    Create scenefinal.js

---
+++ scenefinal.js
@@ -0,0 +1,74 @@
+var i = 0;
+
+
+//array of objects for the properties for sone
+var p = [{
+    greeting: "5 + 4 = ?",
+    scene: 10,
+    one: 12,
+    two: 11,
+    three: 10,
+    four: 9,
+  },
+  {
+    greeting: "12 + 23 = ?",
+    scene: 11,
+    one: 24,
+    two: 25,
+    three: 35,
+    four: 34,
+  },
+  {
+    greeting: "5 + 9 = ?",
+    scene: 12,
+    one: 11,
+    two: 17,
+    three: 15,
+    four: 14,
+  },
+  {
+    greeting: "6 + 3 + 2 = ?",
+    scene: 13,
+    one: 9,
+    two: 11,
+    three: 10,
+    four: 9,
+  },
+  {
+    greeting: "9 + 4 + 6 + 9 = ?",
+    scene: 14,
+    one: 23,
+    two: 27,
+    three: 28,
+    four: 19,
+  },
+  {
+    greeting: "7 + 0 + 2 + 9 = ?",
+    scene: 15,
+    one: 23,
+    two: 19,
+    three: 21,
+    four: 18,
+  },
+  {
+    greeting: "108 + 205 = ?",
+    scene: 16,
+    one: 313,
+    two: 315,
+    three: 308,
+    four: 312,
+  },
+  {
+    greeting: "134 + 74 = ?",
+    scene: 17,
+    one: 212,
+    two: 232,
+    three: 208,
+    four: 214,
+  },
+  {
+    greeting: "Congrats! You have Finished this section!",
+    scene: 18,
+    one: "Type Next to go to the spelling section",
+  }
+];
