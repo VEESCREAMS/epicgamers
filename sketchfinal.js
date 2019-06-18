ec71956047879061c419324630c5a27f459d4ad0
ULT01 <47332107+ULT01@users.noreply.github.com>
2019-06-18 14:14:46 -0400

    Update sketchfinal.js

--- sketchfinal.js
+++ sketchfinal.js
@@ -1,41 +1,38 @@
-// let song;
-
+//defining background image for prescenes
 let img;
+//defining background image for addition levels
 let m;
+//defining background image for pattern levels
 let y;
-
+//defining image for end scene
+let c;
+//defining variable for pattern level class J.S
 let bug;
+//defining image for instructions scene
+let bu;
 
 function setup() {
-   // song = loadSound('assets/lucky_dragons_-_power_melody.mp3');
-  createCanvas(1920, 1200);
-  img = loadImage('20190514_001-5.jpg'); 
-  
+  //creating canvas
+  createCanvas(800, 800);
+  //setting prescenes image
+  img = loadImage('20190514_001-5.jpg');
+  //setting addition levels image
   m = loadImage('20190514_001-4.jpg');
-  
+  //setting pattern levels image
   y = loadImage('20190514_001-3.jpg');
-  
-  se1.draw();
-  btn1.draw();
+  //setting end scene image
+  c = loadImage('Star_.svg.png');
+  //setting image for instructions
+  bu = loadImage('epiclogo_gamer.png');
+  //setting color variations for opening scene
+  colorMode(height, height, height);
+  //defining variable for J.S class for pattern levels
+  delete btn2.x;
+  delete btn3.x;
+  delete btn4.x;
+  delete btn5.x;
+  delete btn6.x;
+
   bug = new Jitter();
-}
-
-function draw() {
-  
-
-  image(m, 0, height / 4, m.width / 4, m.height / 4);
-  
-  image(y, 0, height / 2, img.width / 4, img.height / 4);
-  
- if (scene > 0 && scene  < 5) {
-   bug.move();
-   bug.display();
-   bug.update();
-     image(m, 0, height / 2, m.width / 4, m.height / 4);
-   
-   
- } 
   
 }
-
-
