bd966470e196ef7e6c249602fe6dc2c98d535c78
ULT01 <47332107+ULT01@users.noreply.github.com>
2019-06-21 07:21:37 -0400

    Create mouseclickfinal.js

---
+++ mouseclickfinal.js
@@ -0,0 +1,9 @@
+//checking mouse is clicked inside each button
+mouseClicked = function() {
+  btn1.handleMouseClick();
+  btn2.handleMouseClick();
+  btn3.handleMouseClick();
+  btn4.handleMouseClick();
+  btn5.handleMouseClick();
+  btn6.handleMouseClick();
+};
