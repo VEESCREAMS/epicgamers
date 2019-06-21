622086586200bdaed571b4436c70d404ed0aaca0
ULT01 <47332107+ULT01@users.noreply.github.com>
2019-06-21 07:04:57 -0400

    Update greetfinal.js

--- greetfinal.js
+++ greetfinal.js
@@ -2,57 +2,60 @@
 function greet() {
   const name = input.value();
   input.value('');
+
+  //for each question, not including the transitions scene, the "x" value for button value is restated and the victory scene is called
+
   //question 1
   if (scene === 10 && name === '9') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
-  //question 2
+    //question 2
   } else if (scene === 11 && name === '35') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
-  //question 3
+    //question 3
   } else if (scene === 12 && name === '14') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
-  //question 4
+    //question 4
   } else if (scene === 13 && name === '11') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
-  //question 5
+    //question 5
   } else if (scene === 14 && name === '28') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
     //question 6
   } else if (scene === 15 && name === '18') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
     //question 7
   } else if (scene === 16 && name === '313') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
     //question 8
   } else if (scene === 17 && name === '208') {
-    btn6.x = 400;
+    btn6.x = 600;
     vic();
-    //question 9
+    //Transition scene
   } else if (scene === 18 && name === 'Next') {
     //switching to pattern levels
-    
-    
+
+
     sceneee = 4;
     $("button").remove();
-      $("input").remove();
+    $("input").remove();
 
-      //removing all html texts
-      $("h2").text("");
-      $("h3").text("");
+    //removing all html texts
+    $("h2").text("");
+    $("h3").text("");
     //changing html background colors for html for pattern levels
     $("body").css("background-color", "rgb(255, 71, 26");
     $("html").css("background-color", "rgb(255, 71, 26");
-    
+
     //creating html text to explain instructions for pattern levels
     gree = createElement('h5', 'Solve the Problem and Press the Right Letter');
-    gree.position(600, 300);
-   
+    gree.position(1000, 0);
+
   }
 }
