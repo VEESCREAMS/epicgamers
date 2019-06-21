a6ec5d651d3be8818023f6889a0ee46792150bed
ULT01 <47332107+ULT01@users.noreply.github.com>
2019-06-21 07:01:54 -0400

    Update answerfinal.js

--- answerfinal.js
+++ answerfinal.js
@@ -1,9 +1,9 @@
 //defining answering box for addition game
 var pp = function() {
   //defining vraiables for position for input box for addition levels
-  var o = 250;
-  var t = 950;
-  
+  var o = 900;
+  var t = 630;
+
   //defining input button and its position
   input = createInput();
   input.position(o, t);
@@ -11,10 +11,9 @@ var pp = function() {
   //creating the input box label
   ans = createElement('h3', 'Answer:');
   ans.position(o + 60, t - 65);
-  
+
   //creaing the submit button
   button = createButton('submit');
   button.position(input.x + input.width, t);
   button.mousePressed(greet);
 };
-
